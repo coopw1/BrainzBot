d20de94672350bdc8a4180e04a84b7b2ea0a7d49
coopw1 <48886919+coopw1@users.noreply.github.com>
2023-12-31 04:18:57 -0500

    Add a username field, as it's such a commonly needed item it makes more sense to store it locally.

--- schemas/userData.js
+++ schemas/userData.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 let userData = new Schema({
   userID: String,
   ListenBrainzToken: String,
+  ListenBrainzUsername: String,
 });
 
 module.exports = model("userData", userData);
