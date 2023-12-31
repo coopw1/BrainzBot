0a46de083cb8928a98ecaa21cdf5ea10e70a0cc1
coopw1 <48886919+coopw1@users.noreply.github.com>
2023-12-31 02:23:02 -0500

    Add a check to make sure the token is valid, then send a proper confirmation

--- src/commands/user/login.js
+++ src/commands/user/login.js
@@ -9,24 +9,23 @@ const {
   ApplicationCommandOptionType,
 } = require("discord.js");
 
-const mongoose = require("mongoose");
+const axios = require("axios").default;
 const userData = require("../../../schemas/userData");
 
 module.exports = {
   name: "login",
   description:
     "Gives you a link to connect your ListenBrainz account to brainzbot",
-  // devOnly: Boolean,
-  testOnly: true,
   options: [
     {
       name: "token",
       description: "Enter your ListenBrainz User token",
       type: ApplicationCommandOptionType.String,
-      required: false,
+      required: true,
+      minLength: 36,
+      maxLength: 36,
     },
   ],
-  // deleted: Boolean,
 
   callback: async (client, interaction) => {
     if (!interaction.options.get("token")) {
@@ -56,29 +55,48 @@ module.exports = {
       interaction.reply({ embeds: [embed], components: [row] });
     } else {
       const token = interaction.options.get("token").value;
+      let response;
 
-      // Save token to DB
       try {
-        const user = await userData.findOne({ userID: interaction.user.id });
-        if (user) {
-          await userData.findOneAndUpdate(
-            { userID: interaction.user.id },
-            { ListenBrainzToken: token }
-          );
-        } else {
-          await userData.create({
-            userID: interaction.user.id,
-            ListenBrainzToken: token,
-          });
-        }
+        BASE_URL = "https://api.listenbrainz.org/1/validate-token";
+        AUTH_HEADER = {
+          Authorization: `Token ${token}`,
+        };
+
+        response = await axios.get(BASE_URL, {
+          headers: AUTH_HEADER,
+        });
       } catch (error) {
-        console.log(`Failed to save token to DB: ${error}`);
+        console.log("Error: " + error);
       }
 
-      const embed = new EmbedBuilder()
-        .setDescription("Logged in as " + interaction.user.tag)
-        .setColor("ba0000");
-      interaction.reply({ embeds: [embed] });
+      if (response.data.valid) {
+        // Save token to DB
+        try {
+          const user = await userData.findOne({ userID: interaction.user.id });
+          if (user) {
+            await userData.findOneAndUpdate(
+              { userID: interaction.user.id },
+              { ListenBrainzToken: token }
+            );
+          } else {
+            await userData.create({
+              userID: interaction.user.id,
+              ListenBrainzToken: token,
+            });
+          }
+        } catch (error) {
+          console.log(`Failed to save token to DB: ${error}`);
+        }
+
+        // Send confirmation
+        const embed = new EmbedBuilder()
+          .setDescription(
+            `✅ You have been logged in to .fmbot with the username [${response.data.user_name}](https://listenbrainz.org/user/${response.data.user_name}/)!`
+          )
+          .setColor("32cd32");
+        interaction.reply({ embeds: [embed], ephemeral: true });
+      }
     }
   },
 };
