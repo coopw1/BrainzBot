801791edbd061507561fdad380eaae82025b5b1f
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-01 15:44:22 -0500

    removing "requirment" for adaptable.io

--- src/index.js
+++ src/index.js
@@ -3,19 +3,6 @@ const { Client, IntentsBitField, ActivityType } = require("discord.js");
 const mongoose = require("mongoose");
 const eventHandler = require("./handlers/eventHandler");
 
-const port = process.env.PORT || 3000;
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  // Your request handling logic goes here
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
-});
-
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
-
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
