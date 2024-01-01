17058fe61d4585c3566803b4a2ed0c8f4ee61164
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-01 04:23:46 -0500

    Atempting to use adaptable.io

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ const { Client, IntentsBitField, ActivityType } = require("discord.js");
 const mongoose = require("mongoose");
 const eventHandler = require("./handlers/eventHandler");
 
+const port = process.env.PORT || 3000;
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // Your request handling logic goes here
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -14,7 +27,7 @@ const client = new Client({
 (async () => {
   try {
     mongoose.set("strictQuery", false);
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.DATABASE_URL);
     if (mongoose.connect) {
       console.log("Connected to MongoDB.");
     }
