e99c9e1d3f660f2611d49c1240f9315d920a53d1
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-02 01:35:33 -0500

    adding auth headers, possibly will help with ratelimting?

--- src/commands/general/util/getMBID.js
+++ src/commands/general/util/getMBID.js
@@ -1,12 +1,21 @@
 const axios = require("axios").default;
 
-module.exports = async (artistName, releaseName, trackName) => {
+module.exports = async (
+  artistName,
+  releaseName,
+  trackName,
+  listenBrainzToken
+) => {
   let response;
   try {
     BASE_URL = "http://musicbrainz.org/ws/2/recording/";
+    AUTH_HEADER = {
+      Authorization: `Token ${listenBrainzToken}`,
+    };
     PARAMS = {
       params: {
         query: `artist:${artistName} release:${releaseName}  recording:${trackName}`,
+        headers: AUTH_HEADER,
       },
     };
 
