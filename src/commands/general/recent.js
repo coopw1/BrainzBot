c7917e6556c8cc1d6c3d135513e31c92035ad742
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-02 01:34:32 -0500

    finish pagination and move it to an external function

--- src/commands/general/recent.js
+++ src/commands/general/recent.js
@@ -4,10 +4,13 @@ const {
   ButtonStyle,
   ActionRowBuilder,
   ComponentType,
+  time,
 } = require("discord.js");
 
 const userData = require("../../../schemas/userData");
 const getRecentlyPlayed = require("./util/getRecentlyPlayed");
+const getTotalScrobbles = require("./util/getTotalScrobbles");
+const pagination = require("../util/pagination");
 
 module.exports = {
   name: "recent",
@@ -31,37 +34,80 @@ module.exports = {
         )
         .setColor("ba0000");
       interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
     }
 
     const brainzUsername = currentUserData.ListenBrainzUsername;
     const listenBrainzToken = currentUserData.ListenBrainzToken;
 
-    // Get recently played tracks from ListenBrainz API
-    const recentlyPlayed = getRecentlyPlayed(listenBrainzToken, brainzUsername);
-
     let currentPage = 0;
+    let descriptions = ["", "", "", "", ""];
+
+    // Get recently played tracks from ListenBrainz API
+    const recentlyPlayed = await getRecentlyPlayed(
+      listenBrainzToken,
+      brainzUsername
+    );
+
+    // Check if there are no recently played tracks
+    if (recentlyPlayed.count === 0) {
+      const embed = new EmbedBuilder()
+        .setDescription("❌ You have no recent listens!")
+        .setColor("ba0000");
+      interaction.reply({ embeds: [embed], ephemeral: true });
+      return;
+    }
+    const maxPages = Math.ceil(recentlyPlayed.count / 5);
+
+    // Get data for each listen
+    recentlyPlayed.listens.forEach(async (listen, index) => {
+      // Extract relevant information from the listen object
+      const artistName = listen.track_metadata.artist_name;
+      const releaseName = listen.track_metadata?.release_name;
+      const trackName = listen.track_metadata.track_name;
+      const timestamp = new Date(listen.listened_at * 1000);
+      const MBID = listen.track_metadata?.mbid_mapping?.recording_mbid;
+
+      // Add the data to their corresponding embed
+      if (MBID === undefined) {
+        // No MBID
+        descriptions[Math.floor(index / 5)] =
+          descriptions[Math.floor(index / 5)] +
+          `__**${trackName}**__  by **${artistName}**\n` +
+          `${time(timestamp, "t")} • *${releaseName}*\n\n`;
+      } else {
+        // Has MBID
+        descriptions[Math.floor(index / 5)] =
+          descriptions[Math.floor(index / 5)] +
+          `**[${trackName}](https://musicbrainz.org/recording/${MBID}/)**  by **${artistName}**\n` +
+          `${time(timestamp, "t")} • *${releaseName}*\n\n`;
+      }
+    });
+
+    // Get total scrobbles
+    totalScrobbles = await getTotalScrobbles(listenBrainzToken, brainzUsername);
+    // Create base embed
+    const baseEmbed = {
+      title: `Lastest tracks for ${interaction.user.username}`,
+      color: 0x353070,
+    };
 
     //Create embed for each page, 5 listens each
-    const recent1to5Embed = new EmbedBuilder({
-      color: 0x353070,
-      description: "Your recent listens:",
-    });
-    const recent6to10Embed = new EmbedBuilder({
-      color: 0x353070,
-      description: "Your recent listens:",
-    });
-    const recent11to15Embed = new EmbedBuilder({
-      color: 0x353070,
-      description: "Your recent listens:",
-    });
-    const recent16to20Embed = new EmbedBuilder({
-      color: 0x353070,
-      description: "Your recent listens:",
-    });
-    const recent21to15Embed = new EmbedBuilder({
-      color: 0x353070,
-      description: "Your recent listens:",
-    });
+    let recent1to5Embed = new EmbedBuilder(baseEmbed).setDescription(
+      await descriptions[0]
+    );
+    let recent6to10Embed = new EmbedBuilder(baseEmbed).setDescription(
+      await descriptions[1]
+    );
+    let recent11to15Embed = new EmbedBuilder(baseEmbed).setDescription(
+      await descriptions[2]
+    );
+    let recent16to20Embed = new EmbedBuilder(baseEmbed).setDescription(
+      await descriptions[3]
+    );
+    let recent21to25Embed = new EmbedBuilder(baseEmbed).setDescription(
+      await descriptions[4]
+    );
 
     // Create list of embeds
     const embeds = [
@@ -69,57 +115,9 @@ module.exports = {
       recent6to10Embed,
       recent11to15Embed,
       recent16to20Embed,
-      recent21to15Embed,
+      recent21to25Embed,
     ];
 
-    // Create left and right buttons
-    const leftButton = new ButtonBuilder({
-      customId: "left",
-      label: "Left",
-      style: ButtonStyle.Primary,
-    });
-
-    const rightButton = new ButtonBuilder({
-      customId: "right",
-      label: "Right",
-      style: ButtonStyle.Primary,
-    });
-
-    // Create a row with the buttons
-    const row = new ActionRowBuilder({
-      components: [leftButton, rightButton],
-    });
-
-    // Send embed
-    const message = await interaction.reply({
-      embeds: [embeds[currentPage]],
-      components: [row],
-    });
-
-    // Create a collector that waits for the user to click the button
-    const buttonCollectorFilter = (i) => i.user.id === interaction.user.id;
-    const collector = message.createMessageComponentCollector({
-      ComponentType: ComponentType.Button,
-      filter: buttonCollectorFilter,
-    });
-
-    // Handle the collector
-    collector.on("collect", async (i) => {
-      // Check if the button was continue
-      if (i.customId === "left") {
-        // User clicked left
-        // Check if first page is active to switch to last
-        if (currentPage === 0) {
-          // Switch to last page
-          currentPage = 4;
-        } else {
-          // Switch to previous page
-          currentPage--;
-        }
-        // Edit embed to show previous 5 listens
-        await interaction.editReply({ embeds: [embeds[currentPage]] });
-      } else if (i.customId === "right") {
-      }
-    });
+    pagination(interaction, embeds, maxPages);
   },
 };
---
+++ src/commands/util/pagination.js
@@ -0,0 +1,89 @@
+const {
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  ComponentType,
+} = require("discord.js");
+
+module.exports = async (interaction, embeds, maxPages, currentPage = 0) => {
+  // Create left and right buttons
+  const leftButton = new ButtonBuilder({
+    customId: "left",
+    label: "<",
+    style: ButtonStyle.Primary,
+  });
+
+  const rightButton = new ButtonBuilder({
+    customId: "right",
+    label: ">",
+    style: ButtonStyle.Primary,
+  });
+
+  // Create a row with the buttons
+  const row = new ActionRowBuilder({
+    components: [leftButton, rightButton],
+  });
+
+  // Send embed
+  const message = await interaction.reply({
+    embeds: [
+      embeds[currentPage].setFooter({
+        text: `Page ${currentPage + 1}/${maxPages} - ${
+          interaction.user.username
+        } has ${totalScrobbles} scrobbles`,
+      }),
+    ],
+    components: [row],
+  });
+
+  // Create a collector that waits for the user to click the button
+  const buttonCollectorFilter = (i) => {
+    return i.user.id === interaction.user.id;
+  };
+  const collector = message.createMessageComponentCollector({
+    ComponentType: ComponentType.Button,
+    filter: buttonCollectorFilter,
+  });
+
+  // Handle the collector
+  collector.on("collect", async (i) => {
+    // Check if the button was continue
+    if (i.customId === "left") {
+      i.deferUpdate();
+      // User clicked left
+      // Check if first page is active to switch to last
+      if (currentPage === 0) {
+        // Switch to last page
+        currentPage = maxPages - 1;
+      } else {
+        // Switch to previous page
+        currentPage--;
+      }
+      // Edit embed to show previous 5 listens
+      await interaction.editReply({ embeds: [embeds[currentPage]] });
+    } else if (i.customId === "right") {
+      i.deferUpdate();
+      // User clicked right
+      // Check if last page is active to switch to first
+      if (currentPage === maxPages - 1) {
+        // Switch to first page
+        currentPage = 0;
+      } else {
+        // Switch to next page
+        currentPage++;
+      }
+
+      // Edit embed to show next 5 listens
+      await interaction.editReply({
+        embeds: [
+          embeds[currentPage].setFooter({
+            text: `Page ${currentPage + 1}/${maxPages} - ${
+              interaction.user.username
+            } has ${totalScrobbles} scrobbles`,
+          }),
+        ],
+        components: [row],
+      });
+    }
+  });
+};
