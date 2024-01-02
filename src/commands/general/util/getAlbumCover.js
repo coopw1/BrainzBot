91cfef079d3884e1e1d51940658292e7f7174e10
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-02 00:41:29 -0500

    return null rather than erroring when MBID = undefined

--- src/commands/general/util/getAlbumCover.js
+++ src/commands/general/util/getAlbumCover.js
@@ -16,7 +16,7 @@ module.exports = async (MBID) => {
   } catch (error) {
     console.log("Error: " + error);
   }
-  releaseMBID = response.data.recordings[0].releases[0].id;
+  releaseMBID = response.data.recordings[0]?.releases[0].id;
 
   return `https://coverartarchive.org/release/${releaseMBID}/front`;
 };
