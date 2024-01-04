ad916723da7ea019b32eee1a3bfbeeb310fadc64
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-03 23:37:49 -0500

    Don't need listenbrainz token for a musicbrainz request

--- src/commands/general/util/getMBID.js
+++ src/commands/general/util/getMBID.js
@@ -1,20 +1,11 @@
 const axios = require("axios").default;
 
-module.exports = async (
-  artistName,
-  releaseName,
-  trackName,
-  listenBrainzToken
-) => {
+module.exports = async (artistName, releaseName, trackName) => {
   try {
     const BASE_URL = "http://musicbrainz.org/ws/2/recording/";
-    const AUTH_HEADER = {
-      Authorization: `Token ${listenBrainzToken}`,
-    };
     const PARAMS = {
       params: {
         query: `artist:${artistName} release:${releaseName}  recording:${trackName}`,
-        headers: AUTH_HEADER,
       },
     };
 
