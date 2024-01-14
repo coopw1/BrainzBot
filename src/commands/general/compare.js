7c9731ffc90296a5ca1a4d93751beb3853659131
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-13 19:08:47 -0500

    Modified compare.js removed extra console.log

--- src/commands/general/compare.js
+++ src/commands/general/compare.js
@@ -109,7 +109,6 @@ module.exports = {
       });
       interaction.editReply({ embeds: [embed] });
     } else {
-      console.log(similarity);
       const embed = new EmbedBuilder({
         title: `${brainzUsername} and ${
           interaction.options.get("compareuser").value
