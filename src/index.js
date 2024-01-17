9791cfdb7e43af86b05947e47dcf1e5202c04bfb
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-17 13:24:32 -0500

    Add updating to top.gg

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const { Client, IntentsBitField } = require("discord.js");
 const mongoose = require("mongoose");
 const eventHandler = require("./handlers/eventHandler");
+const { AutoPoster } = require("topgg-autoposter");
 
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages],
 });
+
 (async () => {
   try {
     mongoose.set("strictQuery", false);
@@ -17,6 +19,11 @@ const client = new Client({
     eventHandler(client);
 
     client.login(process.env.TOKEN);
+
+    const ap = AutoPoster(process.env.TOPGG_TOKEN, client);
+    ap.on("posted", () => {
+      console.log("Posted stats to Top.gg!");
+    });
 
     process.on("unhandledRejection", (error) => {
       console.error("Unhandled promise rejection:", error);
