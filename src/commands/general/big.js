0bb288e5d214d3965b3ccf4fa1af190e1ea97830
coopw1 <48886919+coopw1@users.noreply.github.com>
2024-01-21 19:06:01 -0500

    Created big.js and getAllListens.js - Get all of a users listens from all time, and then show it!

---
+++ src/commands/general/big.js
@@ -0,0 +1,113 @@
+const getAuth = require("../util/getAuth");
+const pagination = require("../util/pagination");
+const getAllListens = require("./util/getAllListens");
+
+require("dotenv").config();
+
+const {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  time,
+} = require("discord.js");
+
+module.exports = {
+  name: "big",
+  description: "Big",
+  category: "General",
+  options: [
+    {
+      name: "listens",
+      description: "Get ALL of your listens!",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "user",
+          description: "A ListenBrainz username",
+          type: ApplicationCommandOptionType.String,
+          required: false,
+        },
+      ],
+    },
+  ],
+
+  callback: async (client, interaction) => {
+    const { brainzUsername, listenBrainzToken } = await getAuth(interaction);
+    if (interaction.replied) {
+      return;
+    }
+    if (listenBrainzToken === process.env.LISTENBRAINZ_TOKEN) {
+      const embed = new EmbedBuilder({
+        description:
+          "❌ You must link your ListenBrainz account to use this command!\n" +
+          "Use the </login:1190736297770352801> command to link your ListenBrainz account.",
+        color: 0xba0000,
+      });
+      interaction.editReply({ embeds: [embed] });
+      return;
+    }
+
+    const allListens = await getAllListens(listenBrainzToken, brainzUsername);
+
+    const maxPages = Math.ceil(allListens.count / 10);
+
+    let descriptions = [];
+
+    for (let i = 0; i < maxPages; i++) {
+      descriptions[i] = "";
+    }
+
+    allListens.listens.forEach((listen, index) => {
+      // Extract relevant information from the listen object
+      const artistName = listen.track_metadata.artist_name;
+      const releaseName = listen.track_metadata?.release_name;
+      const trackName = listen.track_metadata.track_name;
+      const timestamp = new Date(listen.listened_at * 1000);
+      const MBID = listen.track_metadata?.mbid_mapping?.recording_mbid;
+
+      // Add the data to their corresponding embed
+      if (MBID === undefined) {
+        // No MBID
+        if (listen.track_metadata.additional_info.origin_url === undefined) {
+          descriptions[Math.floor(index / 10)] =
+            descriptions[Math.floor(index / 10)] +
+            `__**${trackName}**__  by **${artistName}**\n` +
+            `${time(timestamp, "f")} • *${releaseName}*\n\n`;
+        } else {
+          descriptions[Math.floor(index / 10)] =
+            descriptions[Math.floor(index / 10)] +
+            `**[${trackName}](${listen.track_metadata.additional_info.origin_url})** by **${artistName}**\n` +
+            `${time(timestamp, "f")} • *${releaseName}*\n\n`;
+        }
+      } else {
+        // Has MBID
+        if (listen.track_metadata.additional_info.origin_url === undefined) {
+          descriptions[Math.floor(index / 10)] =
+            descriptions[Math.floor(index / 10)] +
+            `**${trackName}** by **${artistName}** [(MB)](https://musicbrainz.org/recording/${MBID}/)\n` +
+            `${time(timestamp, "f")} • *${releaseName}*\n\n`;
+        } else {
+          descriptions[Math.floor(index / 10)] =
+            descriptions[Math.floor(index / 10)] +
+            `**[${trackName}](${listen.track_metadata.additional_info.origin_url})** by **${artistName}** [(MB)](https://musicbrainz.org/recording/${MBID}/)\n` +
+            `${time(timestamp, "f")} • *${releaseName}*\n\n`;
+        }
+      }
+    });
+
+    let baseEmbed = {
+      title: `Full listen history for ${brainzUsername}`,
+      color: 0x353070,
+    };
+
+    let embeds = [];
+    for (let i = 0; i < maxPages; i++) {
+      embeds[i] = new EmbedBuilder(baseEmbed).setDescription(descriptions[i]);
+    }
+    pagination(
+      interaction,
+      embeds,
+      maxPages,
+      ` - ${brainzUsername} has ${allListens.count} scrobbles`
+    );
+  },
+};
---
+++ src/commands/general/util/getAllListens.js
@@ -0,0 +1,69 @@
+const axios = require("axios").default;
+
+/**
+ * Retrieves the all the tracks of a user from the ListenBrainz API.
+ *
+ * @param {string} listenBrainzToken - The authentication token for accessing the ListenBrainz API.
+ * @param {string} brainzUsername - The username of the user whose recently played tracks are to be retrieved.
+ * @return {promise<Object>} An object of recently played tracks.
+ */
+module.exports = async (listenBrainzToken, brainzUsername) => {
+  const BASE_URL = `https://api.listenbrainz.org/1/user/${brainzUsername}/listens`;
+  const AUTH_HEADER = {
+    Authorization: `Token ${listenBrainzToken}`,
+    "User-Agent": "DiscordBrainzBot/1.0.0 (coopwd@skiff.com)",
+  };
+
+  let PARAMS = {
+    headers: AUTH_HEADER,
+    params: {
+      count: 1000,
+      max_ts: 0,
+    },
+  };
+
+  let responses = {
+    count: 0,
+    latest_listen_ts: 0,
+    listens: [],
+    oldest_listen_ts: 0,
+    user_id: "",
+  };
+  let lastResponseCount = 1000;
+  while (lastResponseCount === 1000) {
+    const response = await axios.get(BASE_URL, PARAMS).catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+      return "error";
+    });
+    const lastResponse = response.data.payload;
+
+    responses.count += lastResponse.count;
+
+    let tempListens = responses.listens;
+    responses.listens = tempListens.concat(lastResponse.listens);
+
+    responses.latest_listen_ts = lastResponse.latest_listen_ts;
+    responses.oldest_listen_ts = lastResponse.oldest_listen_ts;
+    responses.user_id = lastResponse.user_id;
+
+    PARAMS.params.max_ts = lastResponse.listens.slice(-1)[0].listened_at;
+
+    lastResponseCount = response.data.payload.count;
+  }
+  return responses;
+};
